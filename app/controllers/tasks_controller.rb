ae61d6a3c39516f45302d50ddf399d10550324ba
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-09 14:00:21 +0000

    resources used in routes.rb, before action used in task controller for find by id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,20 +16,23 @@ class TasksController < ApplicationController
     redirect_to task_path(@new_task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    completed = params[:task][:completed] != '0'
-    @task = Task.find(params[:id])
-    @task.update(title: params[:task][:title], details: params[:task][:details], completed: completed)
+    @task.update(title: params[:task][:title], details: params[:task][:details], completed: params[:task][:completed])
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
+  end
+
+  # def restaurant_params
+  #   params.require(:restaurant).permit(:name, :address, :rating)
+  # end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
