883d9078b0aa8133c67f1c3e2195ad3b80a54ca4
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-09 13:01:44 +0000

    route, view, controller for creating task added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,14 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @new_task = Task.new({ title: params[:task][:title], details: params[:task][:details] })
+    @new_task.save
+    redirect_to task_path(@new_task)
+  end
 end
